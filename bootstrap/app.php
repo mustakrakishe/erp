f33423c345eb1fc487d9b4e6f2fceea21232456b
mustakrakishe <mustakrkish@gmail.com>
2024-06-27 22:38:11 +0300

    Disable unauthorized user redirection

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\ExceptionIfAuthenticated;
 use App\Http\Middleware\ForceJsonResponse;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
@@ -14,7 +15,8 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware
-            ->prependToGroup('api', ForceJsonResponse::class);
+            ->prependToGroup('api', ForceJsonResponse::class)
+            ->alias(['guest' => ExceptionIfAuthenticated::class]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //

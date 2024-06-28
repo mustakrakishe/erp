15e6389fcca4b5d851d1318272b5993e095441f4
mustakrakishe <mustakrkish@gmail.com>
2024-06-29 00:53:56 +0300

    Create url user parameter slug

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Route::bind('user', function (string $value) {
+            return User::findOrFail(
+                $value === 'me' ? request()->user()->id : $value
+            );
+        });
     }
 }

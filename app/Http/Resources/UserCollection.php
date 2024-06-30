148ade80ebaa1e14a82e0619085375c4256a1d55
mustakrakishe <mustakrkish@gmail.com>
2024-06-30 21:53:45 +0300

    Create resource collection

--- app/Http/Resources/UserCollection.php
+++ app/Http/Resources/UserCollection.php
@@ -2,31 +2,6 @@
 
 namespace App\Http\Resources;
 
-use Illuminate\Http\Resources\Json\ResourceCollection;
-
 class UserCollection extends ResourceCollection
 {
-    /**
-     * Customize the pagination information for the resource.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  array $paginated
-     * @param  array $default
-     * @return array
-     */
-    public function paginationInformation($request, $paginated, $default)
-    {
-        return [
-            'meta' => [
-                'total'        => $paginated['total'],
-                'from'         => $paginated['from'],
-                'to'           => $paginated['to'],
-
-                'per_page'     => $paginated['per_page'],
-
-                'current_page' => $paginated['current_page'],
-                'last_page'    => $paginated['last_page'],
-            ],
-        ];
-    }
 }
